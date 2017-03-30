c44664552dfb997d7091d30f425e6a7aa5c430c4
crabnebul <kevin.ziehl@student.kit.edu>
2017-03-30 09:33:04 +0200

    added Tprime

--- exe/Build_Histo_from_Tree.cc
+++ exe/Build_Histo_from_Tree.cc
@@ -19,16 +19,16 @@ using namespace std;
 int main()
 {
 	// create root file in which the histograms will be saved
-	TFile *file = new TFile("/nfs/dust/cms/user/kziehl/plots/bkg_estimation/rootfile/analysis_QCD.root","RECREATE");
+	TFile *file = new TFile("/nfs/dust/cms/user/kziehl/plots/bkg_estimation/rootfile/analysis_TTbar.root","RECREATE");
 	
 	
 	//Load data files
-	TChain	*chain = gRLoader->GetQCDFiles();
+	TChain	*chain = gRLoader->GetTTbarFiles();
 	std::cout << "Number of Entries in chain: " << chain->GetEntries() << std::endl;
 
   
   //variables
-  Float_t Weight_XS{0},TTM_Zprime_M{0},TTM_Mistagrate{0},TTM_separated_highest_bottoms_M{0},TTM_AK8_top_candidates_highest_M{0},TTM_highest_Ws_M{0},TTM_no_top_separated_highest_bottoms_M{0},TTM_no_top_Zprime_M{0},Signal_Topfirst_Zprime_M{0},TTM_Tprime_M{0},TTM_no_top_Tprime_M{0};
+  Float_t Weight_XS{0},TTM_Zprime_M{0},TTM_Mistagrate{0},TTM_separated_highest_bottoms_M{0},TTM_AK8_top_candidates_highest_M{0},TTM_highest_Ws_M{0},TTM_no_top_separated_highest_bottoms_M{0},TTM_no_top_Zprime_M{0},Signal_Topfirst_Zprime_M{0},TTM_Tprime_M{0},TTM_no_top_Tprime_M{0},Signal_Topfirst_Tprime_M{0};
   
   
   //Set branches
@@ -65,7 +65,10 @@ int main()
   chain->SetBranchAddress("TTM_no_top_Tprime_M",&TTM_no_top_Tprime_M);
   
   chain->SetBranchStatus("Signal_Topfirst_Zprime_M",1);
-  chain->SetBranchAddress("Signal_Topfirst_Zprime_M",&Signal_Topfirst_Zprime_M); 
+  chain->SetBranchAddress("Signal_Topfirst_Zprime_M",&Signal_Topfirst_Zprime_M);
+  
+  chain->SetBranchStatus("Signal_Topfirst_Tprime_M",1);
+  chain->SetBranchAddress("Signal_Topfirst_Tprime_M",&Signal_Topfirst_Tprime_M);  
   
   //creating histograms
   TH1F* hZmass = new TH1F("hZmass","M(Z') Background",50,0,5000);
@@ -74,6 +77,7 @@ int main()
   TH1F* hmisstag = new TH1F("hmisstag","misstagrate",100,0,1);
   TH1F* hTmass = new TH1F("hTmass","m_{T'}",50,0,5000);
   TH1F* hTmassnotop = new TH1F("hTmassnotop","m_{T} no top",50,0,5000);
+  TH1F* hTmassTopfirst = new TH1F("hTmassTopfirst","m(t')",50,0,5000);
   //TH1F* hbotmass = new TH1F("hbotmass","Massdistriubtion of b-Quarks with the highest p_{T}",15,0,150);
 	//TH1F* hAK8mass = new TH1F("hAK8mass","Massdistriubtion of AK8 Jets with the highest p_{T}",20,0,200);
 	//TH1F* hWmass = new TH1F("hWmass","Massdistriubtion of W-Bosons with the highest p_{T}",15,0,150);
@@ -86,6 +90,7 @@ int main()
   hZmassnotop->Sumw2();
   hTmass->Sumw2();
   hTmassnotop->Sumw2();
+  hTmassTopfirst->Sumw2();
   
  
  	// sum over entries and fill histogramms
@@ -98,8 +103,9 @@ int main()
 		if(TTM_no_top_Zprime_M>0) hZmassnotop->Fill(TTM_no_top_Zprime_M,Weight_XS*TTM_Mistagrate);
 		if(Signal_Topfirst_Zprime_M>0) hZmassTopfirst->Fill(Signal_Topfirst_Zprime_M,Weight_XS);
 		if(TTM_Mistagrate>0)  hmisstag->Fill(TTM_Mistagrate);
-		if(TTM_Tprime_M>0) hTmass->Fill(TTM_Tprime_M,Weight_XS);
-		if(TTM_no_top_Tprime_M>0) hTmassnotop->Fill(TTM_no_top_Tprime_M,Weight_XS);
+		if(TTM_Tprime_M>0) hTmass->Fill(TTM_Tprime_M,Weight_XS*TTM_Mistagrate);
+		if(TTM_no_top_Tprime_M>0) hTmassnotop->Fill(TTM_no_top_Tprime_M,Weight_XS*TTM_Mistagrate);
+		if(Signal_Topfirst_Tprime_M>0) hTmassTopfirst->Fill(Signal_Topfirst_Tprime_M,Weight_XS);
 		//if(TTM_separated_highest_bottoms_M>0) hbotmass->Fill(iEntry,Weight_XS);
 		//if(TTM_AK8_top_candidates_highest_M>0) hAK8mass->Fill(iEntry,Weight_XS);
 		//if(TTM_highest_Ws_M>0) hWmass->Fill(iEntry,Weight_XS);
@@ -114,12 +120,13 @@ int main()
 
 	//Write and save
   file->cd();
-  hZmass->Write("QCD_TTM_Zprime_M");
-  hZmassTopfirst->Write("QCD_Signal_Topfirst_Zprime_M");
-  hZmassnotop->Write("QCD_TTM_no_top_Zprime_M");
-  hTmass->Write("QCD_TTM_Tprime_M");
-  hTmassnotop->Write("QCD_TTM_no_top_Tprime_M");
-  hmisstag->Write("QCD_misstag");
+  hZmass->Write("TTbar_TTM_Zprime_M");
+  hZmassTopfirst->Write("TTbar_Signal_Topfirst_Zprime_M");
+  hZmassnotop->Write("TTbar_TTM_no_top_Zprime_M");
+  hTmass->Write("TTbar_TTM_Tprime_M");
+  hTmassnotop->Write("TTbar_TTM_no_top_Tprime_M");
+  hmisstag->Write("TTbar_misstag");
+  hTmassTopfirst->Write("TTbar_Signal_Topfirst_Tprime_M");
   file->Close();
   
   
