1b0bf15e5e8c9a291bcee2fc65512b89b1fe6d7c
kziehl <kevin.ziehl@student.kit.edu>
2017-04-03 16:59:43 +0200

    for top

--- exe/Ratioplots.C
+++ exe/Ratioplots.C
@@ -12,22 +12,22 @@
 void Ratioplots()
 {
 	//gRStyle->SetGlobalStyle();
-	TFile *file = new TFile("/home/kevin/Desktop/analysis_QCD.root");
+	TFile *file = new TFile("/home/kevin/Desktop/file/analysis.root");
 	
-	TH1F* h1 = dynamic_cast<TH1F*>(file->Get("QCD_TTM_Zprime_M"));
-	TH1F* h2 = dynamic_cast<TH1F*>(file->Get("QCD_Signal_Topfirst_Zprime_M"));
+	TH1F* h1 = dynamic_cast<TH1F*>(file->Get("QCD_TTM_AK8_top_candidates_highest_pt"));
+	TH1F* h2 = dynamic_cast<TH1F*>(file->Get("QCD_Signal_Topfirst_Tops_Pt"));
 	TH1F* h3 = dynamic_cast<TH1F*>(file->Get("QCD_TTM_no_top_Zprime_M"));
 	TH1F* h4 = dynamic_cast<TH1F*>(file->Get("QCD_misstag"));
-	h1->Scale(1/h1->Integral());
-	h2->Scale(1/h2->Integral());
+	//h1->Scale(1/h1->Integral());
+	//h2->Scale(1/h2->Integral());
 	
 	gRUtil->PrintRatioPlot(h1,h2);
 	//gRUtil->PrintRatioPlot(h1,h3);
 	//gRUtil->PrintRatioPlot(h3,h2);
 	//h1->SetTitle("A simple Ratio");
 
-	gRStyle->BuildLegend(h1,"QCD Prediction","l","RightUp",2);
-	gRStyle->BuildLegend(h2,"QCD Mistag","l");
+	gRStyle->BuildLegend(h1,"QCD AK8 Jets","l","RightUp",2);
+	gRStyle->BuildLegend(h2,"QCD Top","l");
 
 	
 }
